1e53275d536d6ffdf2c3e2fb2e1dc5732165895a
kidusm001 <kidusmesfin27@gmail.com>
2025-04-15 22:27:17 +0300

    refactor: cell constractor and draw method

--- src/geometry.py
+++ src/geometry.py
@@ -12,3 +12,44 @@ class Line():
 
     def draw(self, canvas: Canvas, fill_color="black"):
         canvas.create_line(self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2)
+
+class Cell():
+    def __init__(self, win):
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            left_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(left_wall)
+        if self.has_right_wall:
+            right_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(right_wall)
+        if self.has_top_wall:
+            top_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(top_wall)
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(bottom_wall)
+        
+    def draw_move(self, to_cell, undo=False):
+        own_x_mid = (self._x1 + self._x2) / 2
+        own_y_mid = (self._y1 + self._y2) / 2
+        to_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        to_y_mid = (to_cell._y1 + to_cell._y2) / 2
+        move_line = Line(Point(own_x_mid, own_y_mid), Point(to_x_mid, to_y_mid))
+        fill_color = "gray" if undo else "red"
+        self._win.draw_line(move_line, fill_color)
+
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 from window import Window
-from geometry import Line, Point
+from geometry import Line, Point, Cell
 
 def main():
     win = Window(800,800)
     p1 = Point(1, 1)
-    p2 = Point(20, 22)
-    line = Line(p1, p2)
-    win.draw_line(line, "black")
+    p2 = Point(20, 20)
+    cell = Cell(win)
+    cell.draw(p1.x, p1.y, p2.x, p2.y)
+    p3 = Point(100, 100)
+    p4 = Point(20, 20)
+    cell1 = Cell(win)
+    cell1.draw(p4.x, p4.y, p3.x, p3.y)
+    cell.draw_move(cell1)
+    cell2 = Cell(win)
+    cell2.draw(20,1,40,20)
+    cell.draw_move(cell2, True)
+
     win.wait_for_close()
 
 if __name__ == "__main__":

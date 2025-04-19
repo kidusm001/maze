370c1ee0b5cd3edd764acb2478eb5fdfda9160e7
kidusm001 <kidusmesfin27@gmail.com>
2025-04-19 17:19:56 +0300

    solver done

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 from window import Window
-from geometry import Line, Point, Cell
 from maze import Maze
+import sys
 
 def main():
-    win = Window(800,800)
-    maze = Maze(10, 10, 5,10, 20, 20, win, 0)
-    
+    num_rows = 20
+    num_cols = 45
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = 20
+    cell_size_y = 20
 
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("GGs ez")
+    else:
+        print("solved")
     win.wait_for_close()
 
 if __name__ == "__main__":
